ec19aa63d992ae63c0c0464f75c5a39eb746fef9
Melba17 <evabancerowski77@gmail.com>
2024-06-02 17:54:43 +0200

    visuel modale

--- Develop/README.md
+++ Develop/README.md
@@ -3,3 +3,15 @@
 2. Il est conseillé d'utiliser VisualStudio Code et vous pouvez utiliser Docker, mais ce n'est pas obligatoire ;
 3. Il n'y a aucune dépendance ;
 4. Vous ne devez utiliser que du CSS personnalisé et du JavaScript pur, sans jQuery, Bootstrap ou autre librairie.
+
+Projet 4 - GameOn
+
+Créez une landing page avec Javascript.
+Site de GameOn, une entreprise de gaming.
+Programmer avec Javascript à partir zéro.
+
+Utilisation de Figma pour accéder aux maquettes du projet, Visual Studio Code pour l'écriture et la gestion du code, et GitHub pour le versionnement de votre projet.
+
+Travail sur la validation du formulaire pour garantir que les données saisies par les utilisateurs sont correctes et répondent aux critères établis.
+
+Gestion des événements JavaScript, tels que les clics de bouton ou les entrées de formulaire, pour créer une interaction dynamique avec les utilisateurs.
--- Develop/scripts/modal.js
+++ Develop/scripts/modal.js
@@ -1,23 +1,25 @@
 // VARIABLES POUR LA MODALE
 // Sélection du fond sur lequel s'affiche la Modale.
 const modalbg = document.querySelector(".bground");
-// Sélection du bouton unique pour ouvrir la modale.
-const modalBtn = document.querySelector(".modal-btn");
+// Sélection des boutons pour ouvrir la modale.
+const modalBtn = document.querySelectorAll(".modal-btn");
 // Sélection de l'élément de fermeture de la Modale, ici, la croix.
 const closeBtn = document.querySelector(".close");
 
 // GESTIONNAIRES D'ÉVÈNEMENTS
-// Ajout de l'événement click au bouton
-modalBtn.addEventListener("click", launchModal);
-// Fonction pour lancer la modale - elle change le style de l'élément modalbg pour le rendre visible en définissant sa propriété display à block.
+// Ajout de l'événement click aux boutons
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 // Ajout d'un gestionnaire d'événements pour écouter le clic sur la croix.
 closeBtn.addEventListener("click", closeModal);
 
-// FONCTION POUR OUVRIR LA MODALE
-// En résumé, la modale s'ouvre au clic parce que la fonction launchModal() est attachée à l'événement click du bouton via un gestionnaire d'événements. Lorsque le bouton avec la classe modal-btn est cliqué, l'événement click est déclenché. Comme la fonction launchModal a été assignée comme gestionnaire de cet événement, elle est automatiquement appelée. 
+// FONCTION POUR OUVRIR LA MODALE 
+// Fonction pour lancer la modale - elle change le style de l'élément modalbg pour le rendre visible en définissant sa propriété display à block.
+// En résumé, la modale s'ouvre au clic parce que la fonction launchModal() est attachée à l'événement click du bouton via un gestionnaire d'événements. Lorsque le bouton avec la classe modal-btn est cliqué, l'événement click est déclenché. Comme la fonction launchModal a été assignée comme gestionnaire de cet événement, elle est automatiquement appelée.
 function launchModal() {
   modalbg.style.display = "block";
 }
+
+
 // FONCTION POUR FERMER LA MODALE
 function closeModal() {
   modalbg.style.display = "none";
